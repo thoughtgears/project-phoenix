19661a26094a5c9e2105b1d492323029cc953cde
Jens Skott <jens.skott@checkatrade.com>
2024-06-26 22:14:40 +0100

    have fixed all permissions now

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -5,10 +5,16 @@ resource "google_service_account" "phoenix_cloud_function" {
   description  = "Service account for the Phoenix Cloud Function"
 }
 
-resource "google_project_iam_member" "cloud_function_artifact_registry_reader" {
+resource "google_project_iam_member" "phoenix_service_agent" {
   project = module.projects["source"].id
   member  = "serviceAccount:${google_service_account.phoenix_cloud_function.email}"
-  role    = "roles/artifactregistry.reader"
+  role    = "roles/cloudfunctions.serviceAgent"
+}
+
+resource "google_project_iam_member" "cloud_function_artifact_registry_writer" {
+  project = module.projects["source"].id
+  member  = "serviceAccount:${google_service_account.phoenix_cloud_function.email}"
+  role    = "roles/artifactregistry.writer"
 }
 
 resource "google_service_account" "cloud_scheduler_phoenix_backup" {
@@ -25,8 +31,32 @@ resource "google_project_iam_member" "cloud_scheduler_function_invoker" {
 }
 
 // Grant access for the build to pull the source for cloud function on deploy
-resource "google_project_iam_member" "cloud_function_object_viewer" {
+resource "google_project_iam_member" "build_object_viewer" {
   project = module.projects["source"].id
   member  = "serviceAccount:${module.projects["source"].number}-compute@developer.gserviceaccount.com"
   role    = "roles/storage.objectViewer"
+}
+
+resource "google_project_iam_member" "build_log_writer" {
+  project = module.projects["source"].id
+  member  = "serviceAccount:${module.projects["source"].number}-compute@developer.gserviceaccount.com"
+  role    = "roles/logging.logWriter"
+}
+
+resource "google_project_iam_member" "build_cloud_functions_developer" {
+  project = module.projects["source"].id
+  member  = "serviceAccount:${module.projects["source"].number}-compute@developer.gserviceaccount.com"
+  role    = "roles/cloudfunctions.developer"
+}
+
+resource "google_project_iam_member" "build_service_account_user" {
+  project = module.projects["source"].id
+  member  = "serviceAccount:${module.projects["source"].number}-compute@developer.gserviceaccount.com"
+  role    = "roles/iam.serviceAccountUser"
+}
+
+resource "google_project_iam_member" "build_artifact_registry_writer" {
+  project = module.projects["source"].id
+  member  = "serviceAccount:${module.projects["source"].number}-compute@developer.gserviceaccount.com"
+  role    = "roles/artifactregistry.writer"
 }
